4859892615938a7a24d801ff4beb9793788d3a02
jongvvon <jwjang7024@gmail.com>
2024-02-07 19:55:16 +0900

    add change property

--- dynamoDB_func.py
+++ dynamoDB_func.py
@@ -71,25 +71,23 @@ def get_item(table, key):
     item = response['Item']
     print("조회된 항목:", json.dumps(item, indent=4))
 
-# # 사용자로부터 항목의 키와 새 값을 입력받아 해당 항목을 테이블에서 업데이트하는 함수입니다.
-# def update_item(table, key):
-#     item_key = input("업데이트할 항목 키 값을 입력해주세요: ")
-#     item_value = input("새로운 항목 값을 입력해주세요: ")
+# 사용자로부터 항목의 키와 새 값을 입력받아 해당 항목을 테이블에서 업데이트하는 함수입니다.
+def update_item(table, key):
+    item_key = input("접근할 항목 키 값을 입력해주세요: ")
+    property_key = input("변경할 속성 키를 입력해주세요: ")
+    item_value = input("속성의 새로운 항목 값을 입력해주세요: ")
 
-#     table.update_item(
-#         Key={
-#             key: item_key
-#         },
-#         #DynamoDB에서는 특정 단어들을 예약어로 간주하며, 이들은 속성 이름으로 사용할 수 없습니다. 오류 메시지에서 볼 수 있듯이, 'value'는 DynamoDB의 예약어 중 하나입니다.
-#         #이 문제를 해결하려면 ExpressionAttributeNames 매개변수를 사용하여 속성 이름을 대체해야 합니다. 예를 들어, 'value' 대신 '#v'를 사용하려면 다음과 같이 코드를 수정할 수 있습니다:
-#         UpdateExpression='SET #v = :val',
-#         ExpressionAttributeNames={
-#             '#v': 'value'
-#         },
-#         ExpressionAttributeValues={
-#             ':val': item_value
-#         }
-#     )
+    table.update_item(
+        Key={
+            key: item_key
+        },
+        #DynamoDB에서는 특정 단어들을 예약어로 간주하며, 이들은 속성 이름으로 사용할 수 없습니다. 오류 메시지에서 볼 수 있듯이, 'value'는 DynamoDB의 예약어 중 하나입니다.
+        #이 문제를 해결하려면 ExpressionAttributeNames 매개변수를 사용하여 속성 이름을 대체해야 합니다. 예를 들어, 'value' 대신 '#v'를 사용하려면 다음과 같이 코드를 수정할 수 있습니다:
+        UpdateExpression=f'SET {property_key} = :val',
+        ExpressionAttributeValues={
+            ':val': item_value
+        }
+    )
 
 def delete_item(table, key):
     item_key = input("삭제할 항목 키 값을 입력해주세요: ")
@@ -105,7 +103,7 @@ def delete_table(table):
     table.delete()
 
 while True:
-    command = input("명령어를 입력해주세요 (g: 테이블 선택, s: 테이블 전체 조회, r: 항목 조회, di: 아이템 삭제, dt: 테이블 삭제, q: 종료): ")
+    command = input("명령어를 입력해주세요 (g: 테이블 선택, s: 테이블 전체 조회, r: 항목 조회, u: 속성값 변경, di: 아이템 삭제, dt: 테이블 삭제, q: 종료): ")
 
     if command == 'g':
         table, key = get_or_create_table(dynamodb)
@@ -122,14 +120,13 @@ while True:
     elif command == 'r':
         if 'table' in locals():
             get_item(table, key)
-            print(table)
         else:
             print("테이블을 먼저 선택해주세요.")
-    # elif command == 'u':
-    #     if 'table' in locals():
-    #         update_item(table, key)
-    #     else:
-    #         print("테이블을 먼저 선택해주세요.")
+    elif command == 'u':
+        if 'table' in locals():
+            update_item(table, key)
+        else:
+            print("테이블을 먼저 선택해주세요.")
     elif command == 'di':
         if 'table' in locals():
             delete_item(table, key)
@@ -145,3 +142,6 @@ while True:
         break
     else:
         print("잘못된 명령어입니다.")
+    
+    scan_table(table)
+
