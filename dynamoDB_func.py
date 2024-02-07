9a74b9d7e2e31d9d077c2f7c026ccfe4d2c261a0
jongvvon <jwjang7024@gmail.com>
2024-02-07 19:41:02 +0900

    scan & get functions

--- dynamoDB_func.py
+++ dynamoDB_func.py
@@ -1,5 +1,6 @@
 import boto3
 from botocore.exceptions import ClientError
+import json
 
 # DynamoDB 리소스 객체를 생성합니다.
 dynamodb = boto3.resource('dynamodb')
@@ -13,6 +14,7 @@ def get_or_create_table(dynamodb):
     try:
         table = dynamodb.Table(table_name)
         table.load()  # 테이블이 존재하는지 확인합니다.
+        print(table_name + "에 연결되었습니다.")
     except ClientError as e:
         # ResourceNotFoundException이 발생하면 테이블을 새로 생성합니다.
         if e.response['Error']['Code'] == 'ResourceNotFoundException':
@@ -36,44 +38,65 @@ def get_or_create_table(dynamodb):
                 }
             )
 
-    return table
+    return table, primary_key
 
-# 사용자로부터 항목의 키와 값을 입력받아 해당 항목을 테이블에 삽입하는 함수입니다.
-def put_item(table):
-    item_key = input("항목 키 값을 입력해주세요: ")
-    item_value = input("항목 값을 입력해주세요: ")
+# 테이블 전체 조회
+def scan_table(table):
+    # 테이블 스캔
+    response = table.scan()
+    # 스캔 결과를 JSON 형식으로 출력
+    print(json.dumps(response["Items"], indent=4))
 
-    table.put_item(
-        Item={
-            'id': item_key,
-            'value': item_value
-        }
-    )
+
+# # 사용자로부터 항목의 키와 값을 입력받아 해당 항목을 테이블에 삽입하는 함수입니다.
+# def put_item(table):
+#     item_key = input("항목 키 값을 입력해주세요: ")
+#     item_value = input("항목 값을 입력해주세요: ")
+
+#     table.put_item(
+#         Item={
+#             item_key : item_value
+#         }
+#     )
 
 # 사용자로부터 항목의 키를 입력받아 해당 항목을 테이블에서 조회하는 함수입니다.
-def get_item(table):
+def get_item(table, key):
     item_key = input("조회할 항목 키 값을 입력해주세요: ")
 
     response = table.get_item(
         Key={
-            'id': item_key
+            key: item_key
         }
     )
     item = response['Item']
-    print("조회된 항목:", item)
+    print("조회된 항목:", json.dumps(item, indent=4))
 
-# 사용자로부터 항목의 키와 새 값을 입력받아 해당 항목을 테이블에서 업데이트하는 함수입니다.
-def update_item(table):
-    item_key = input("업데이트할 항목 키 값을 입력해주세요: ")
-    item_value = input("새로운 항목 값을 입력해주세요: ")
+# # 사용자로부터 항목의 키와 새 값을 입력받아 해당 항목을 테이블에서 업데이트하는 함수입니다.
+# def update_item(table, key):
+#     item_key = input("업데이트할 항목 키 값을 입력해주세요: ")
+#     item_value = input("새로운 항목 값을 입력해주세요: ")
 
-    table.update_item(
+#     table.update_item(
+#         Key={
+#             key: item_key
+#         },
+#         #DynamoDB에서는 특정 단어들을 예약어로 간주하며, 이들은 속성 이름으로 사용할 수 없습니다. 오류 메시지에서 볼 수 있듯이, 'value'는 DynamoDB의 예약어 중 하나입니다.
+#         #이 문제를 해결하려면 ExpressionAttributeNames 매개변수를 사용하여 속성 이름을 대체해야 합니다. 예를 들어, 'value' 대신 '#v'를 사용하려면 다음과 같이 코드를 수정할 수 있습니다:
+#         UpdateExpression='SET #v = :val',
+#         ExpressionAttributeNames={
+#             '#v': 'value'
+#         },
+#         ExpressionAttributeValues={
+#             ':val': item_value
+#         }
+#     )
+
+def delete_item(table, key):
+    item_key = input("삭제할 항목 키 값을 입력해주세요: ")
+
+    table.delete_item(
         Key={
-            'id': item_key
-        },
-        UpdateExpression='SET value = :val',
-        ExpressionAttributeValues={
-            ':val': item_value
+            key: item_key
         }
     )
 
@@ -82,26 +105,37 @@ def delete_table(table):
     table.delete()
 
 while True:
-    command = input("명령어를 입력해주세요 (g: 테이블 선택, p: 항목 삽입, r: 항목 조회, u: 항목 업데이트, d: 테이블 삭제, q: 종료): ")
+    command = input("명령어를 입력해주세요 (g: 테이블 선택, s: 테이블 전체 조회, r: 항목 조회, di: 아이템 삭제, dt: 테이블 삭제, q: 종료): ")
 
     if command == 'g':
-        table = get_or_create_table(dynamodb)
-    elif command == 'p':
+        table, key = get_or_create_table(dynamodb)
+    # elif command == 'p':
+    #     if 'table' in locals():
+    #         put_item(table)
+    #     else:
+    #         print("테이블을 먼저 선택해주세요.")
+    elif command == 's':
         if 'table' in locals():
-            put_item(table)
+            scan_table(table)
         else:
             print("테이블을 먼저 선택해주세요.")
     elif command == 'r':
         if 'table' in locals():
-            get_item(table)
+            get_item(table, key)
+            print(table)
         else:
             print("테이블을 먼저 선택해주세요.")
-    elif command == 'u':
+    # elif command == 'u':
+    #     if 'table' in locals():
+    #         update_item(table, key)
+    #     else:
+    #         print("테이블을 먼저 선택해주세요.")
+    elif command == 'di':
         if 'table' in locals():
-            update_item(table)
+            delete_item(table, key)
         else:
             print("테이블을 먼저 선택해주세요.")
-    elif command == 'd':
+    elif command == 'dt':
         if 'table' in locals():
             delete_table(table)
             del table
